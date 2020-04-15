2f7f58bb304e6b23d2cd6414743d11ccc6332d00
yvanme <842323064@qq.com>
2020-04-15 19:14:06 +0800

    LinkList

--- jdk8-sample/src/test/java/com/loong/LinkedListTest.java
+++ jdk8-sample/src/test/java/com/loong/LinkedListTest.java
@@ -10,11 +10,19 @@ import java.util.LinkedList;
  * @Description
  * @create 2020-04-15 18:04:30
  **/
+
+/**
+ * 有序,可重复
+ * 优点: 底层数据结构是链表，查询慢，增删快。
+ * 缺点: 线程不安全，效率高
+ */
 public class LinkedListTest {
+
     @Test
     public void test01() {
         LinkedList linkedList = new LinkedList();
         linkedList.add(null);
+        linkedList.add(1);
         linkedList.add(1);
         linkedList.add(2);
         linkedList.add(3);
