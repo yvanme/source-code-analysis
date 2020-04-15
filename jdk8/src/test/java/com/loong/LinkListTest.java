5276f840e8dbab8d1ff32fc889ca0379d4112e86
yvanme <842323064@qq.com>
2020-04-15 18:06:04 +0800

    LinkList

---
+++ jdk8/src/test/java/com/loong/LinkListTest.java
@@ -0,0 +1,16 @@
+package com.loong;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author yvanme
+ * @version 1.0
+ * @Description
+ * @create 2020-04-15 18:04:30
+ **/
+public class LinkListTest {
+    @Test
+    public void test01() {
+
+    }
+}
